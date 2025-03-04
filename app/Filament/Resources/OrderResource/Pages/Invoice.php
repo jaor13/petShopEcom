9cac08e20835769c7096efb283dc07f31ed9bda1
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2025-03-05 00:22:24 +0800

    working invoice

--- app/Filament/Resources/OrderResource/Pages/Invoice.php
+++ app/Filament/Resources/OrderResource/Pages/Invoice.php
@@ -5,11 +5,32 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use Filament\Resources\Pages\Page;
 use App\Models\Order;
+use Filament\Pages\Actions\Action;
 
 class Invoice extends Page
 {
     protected static string $resource = OrderResource::class;
 
+
+    public $record;
+    public $order;
+
+    public function mount($record)
+    {
+        $this->record = $record;
+        $this->order = Order::with(['items.product', 'items.variant', 'address'])->find($record);
+    }
+
+    public function getHeaderActions(): array
+    {
+        return [
+            Action::make('print')
+                ->label('Print')
+                ->icon('heroicon-o-printer')
+                ->requiresConfirmation()
+        ];
+    }
+    
 
     protected static string $view = 'filament.resources.order-resource.pages.invoice';
 
