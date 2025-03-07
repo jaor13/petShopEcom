35389d760247b6a0e318dca0f032f419e19202dd
lladonegrace <gabl2022-1866-69048@bicol-u.edu.ph>
2025-03-08 00:20:16 +0800

    changefooter

--- resources/views/livewire/partials/footer.blade.php
+++ resources/views/livewire/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="container-fluid custom-footer text-white py-3 mt-auto">
     <div class="container text-center">
-        <p class="m-0">&copy; 2024 MyWebsite. All rights reserved.</p>
+        <p class="m-0">&copy; 2025 Aricuz Petshop. All rights reserved.</p>
     </div>
 </footer>
