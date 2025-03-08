a3e4c30c21af8e16a22c3ea5f8d5aaaa6bbf9e55
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-08 17:37:46 +0800

    _breakpoint

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,16 +1,18 @@
 <div>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
     <div class="chatbox_header">
-payo
+        payo
     </div>
 
     <div class="chatbox_body">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quam dolor culpa. Ipsa, id eos aperiam sunt minima animi, temporibus dignissimos molestias obcaecati vero reiciendis dolorem consequatur fugiat accusantium repudiandae!
-<br>Lorem ipsum dolor sit amet.
-<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, praesentium?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quam dolor culpa. Ipsa, id eos aperiam sunt
+        minima animi, temporibus dignissimos molestias obcaecati vero reiciendis dolorem consequatur fugiat accusantium
+        repudiandae!
+        <br>Lorem ipsum dolor sit amet.
+        <br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, praesentium?
     </div>
 
     <div class="chatbox_footer">
-footer
+        footer
     </div>
 </div>
