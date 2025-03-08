3c61faebe205eeec80dc476d7e1c287b89c26e93
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-08 19:52:14 +0800

    modified chat-listblade

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -3,17 +3,45 @@
 
     <div class="chatlist_header">
 
-    <div class="title">
-    Chat
+        <div class="title">
+            Chat
 
-    </div>
+        </div>
 
-    <div class="img_container">
-        <img src="https://picsum.photos/id/237/200/300" alt="dogo">
-    </div>
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="dogo">
+        </div>
     </div>
 
     <div class="chatlist_body">
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
 
+                <img src="https://picsum.photos/id/12/200/300" alt="dogo">
+            </div>
+
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username"> Luse </div>
+                    <span class="date">2d</span>
+                </div>
+
+                <div class="bottom_row">
+
+                    <div class="message_body">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam excepturi reprehenderit
+                        doloremque id explicabo voluptatum assumenda commodi ipsa harum fugit, ut facere. Quae
+                        temporibus est blanditiis amet inventore. Minus repellat placeat dolorem minima iste nesciunt!
+                        Hic provident incidunt ipsum perferendis, eaque odit doloribus amet odio voluptas, consequatur
+                        esse nobis officia!
+                    </div>
+
+                    <div class="unread_count">
+                        56
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </div>
