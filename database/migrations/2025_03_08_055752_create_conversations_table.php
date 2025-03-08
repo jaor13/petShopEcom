fa6210de99d347f997bce77adf6d6d2e088489bd
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-08 14:04:18 +0800

    update create_conversations_table

--- database/migrations/2025_03_08_055752_create_conversations_table.php
+++ database/migrations/2025_03_08_055752_create_conversations_table.php
@@ -4,24 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateConversationsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->timestamp('last_time_message');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('conversations');
     }
-};
+}
