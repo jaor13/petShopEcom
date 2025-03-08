6dff02b9d7017b75cc28cf4fcd63f6acce4f7818
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-08 15:09:27 +0800

    added @livewire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Custom CSS -->
     <link href="{{ asset('assets/css/layout.css') }}" rel="stylesheet">  
+    @livewireStyles
 </head>
 
 <body class="d-flex flex-column min-vh-100 mt-5">
@@ -38,6 +39,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    @livewireScripts
 </body>
 
 </html>
--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,3 +1,8 @@
 <div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 </div>
