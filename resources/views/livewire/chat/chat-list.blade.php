c443ba52462d1c8cdcadfc80ce6940c042a4adef
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-08 19:38:20 +0800

    import and adjustments

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -3,6 +3,14 @@
 
     <div class="chatlist_header">
 
+    <div class="title">
+    Chat
+
+    </div>
+
+    <div class="img_container">
+        <img src="https://picsum.photos/id/237/200/300" alt="dogo">
+    </div>
     </div>
 
     <div class="chatlist_body">
