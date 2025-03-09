4919e14daee7b8c453463fa1749c24a8712f96fd
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-09 15:21:38 +0800

    update create-chat.blade

--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,8 +1,12 @@
 <div>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Users') }}
-        </h2>
-    </x-slot>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+    <ul class="list-group w-75 mx-auto mt-3 container-fluid">
+
+    <li class="list-group-item list-group-item-action">Grace</li>
+    <li class="list-group-item list-group-item-action">Grace</li>
+    <li class="list-group-item list-group-item-action">Grace</li>
+    <li class="list-group-item list-group-item-action">Grace</li>
+    
+    </ul>
 </div>
