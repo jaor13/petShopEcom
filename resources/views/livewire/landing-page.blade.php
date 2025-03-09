d5e9fddadb11de77ff23482f437356dd90d8e18b
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2025-03-09 17:45:35 +0800

    fix styling product grid, landing page

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Afacad:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
 
     <link href="{{ asset('assets/css/layout.css') }}" rel="stylesheet">
     <!-- Main Content -->
