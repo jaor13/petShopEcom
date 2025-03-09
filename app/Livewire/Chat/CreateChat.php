75f807e0505c1736848c8d5030222f28f232fac8
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-09 15:50:09 +0800

    create and show users

--- app/Livewire/Chat/CreateChat.php
+++ app/Livewire/Chat/CreateChat.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\Chat;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use App\Models\User;
 
 class CreateChat extends Component
 {
+    public $users;
     public function render()
     {
+
+        $this->users= User::where('id','!=',auth()->user()->id)->get();
         return view('livewire.chat.create-chat');
     }
 }
--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,12 +1,17 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 
+
+    @foreach ($users as $user)
+
+
+
     <ul class="list-group w-75 mx-auto mt-3 container-fluid">
 
-    <li class="list-group-item list-group-item-action">Grace</li>
-    <li class="list-group-item list-group-item-action">Grace</li>
-    <li class="list-group-item list-group-item-action">Grace</li>
-    <li class="list-group-item list-group-item-action">Grace</li>
+    <li class="list-group-item list-group-item-action">{{$user->username}}</li>
     
     </ul>
+
+    @endforeach
+
 </div>
