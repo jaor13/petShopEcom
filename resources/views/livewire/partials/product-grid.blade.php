78f7b1b77f49b2b7d49c9a19e034814f91da93f3
lladonegrace <gabl2022-1866-69048@bicol-u.edu.ph>
2025-03-10 10:10:38 +0800

    fixcard

--- resources/views/livewire/partials/product-grid.blade.php
+++ resources/views/livewire/partials/product-grid.blade.php
@@ -49,25 +49,27 @@
           <a href="{{ url('product/' . $product->slug) }}">
             <img src="{{ url('storage/' . $product->images[0]) }}" class="card-img-top" alt="{{ $product->product_name }}" style="object-fit: cover; height: 200px;">
           </a>
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title fw-bold">{{ $product->product_name }}</h5>
-            <p class="card-text text-danger fw-bold">₱ {{ number_format($product->price, 2) }}</p>
-            <div class="d-flex align-items-center">
-            <span class="me-1 text-muted" style="margin-left:-4px">⭐ 3.5</span>
-            <span class="text-muted">| 200 sold</span>
-            </div>
-            <p class="text-muted">
-  <i class="fa-solid fa-location-dot" style="opacity: 0.7; margin-right: 4px;"></i> A.A Berces Street San Juan
-</p>
-
-            <div class="mt-">
-            <div style="display: inline-flex; align-items: center; justify-content: center; width: 40px; height: 40px; background-color: #00CED1; border-radius: 10px; padding: 10px;">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-cart3" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-        </svg>
+          <h5 class="card-title fw-bold mt-2">{{ $product->product_name }}</h5>
+                <div style="display: flex; justify-content: space-between; align-items: center; padding: 10px; width: 210px; position: relative;"> 
+    <div>
+        
+        <div style="color: red; font-weight: bold;">₱ {{ number_format($product->price, 2) }}</div>
+        <div>
+            <span style="color: gold;">★</span> 4.5 | 300 sold
+        </div>
+        <div style="display: flex; align-items: center; white-space: nowrap;">
+            <i class="fa-solid fa-location-dot" style="margin-right: 5px; opacity: 0.7;"></i>
+            <span style="font-size: 12px;">A.A Berces Street San Juan</span>
+        </div>
     </div>
-            </div>
-          </div>
+    <div style="position: absolute; bottom: 25px; right: 10px;"> 
+        <div style="display: inline-flex; align-items: center; justify-content: center; width: 40px; height: 40px; background-color: #00CED1; border-radius: 10px; padding: 10px;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-cart3" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+        </div>
+    </div>
+</div>
         </div>
       </div>
       @endforeach
