3ff72d9ebd07e77b4af34ea6bb86048cff229776
Laiza <hediphonextra@gmail.com>
2025-03-12 05:43:46 +0800

    remove liked product from table

--- app/Livewire/LikedProduct.php
+++ app/Livewire/LikedProduct.php
@@ -2,9 +2,13 @@
 
 namespace App\Livewire;
 
+use App\Helpers\LikedProductManagement;
 use App\Models\LikedProducts;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
+
+#[Title("Liked Products - Aricuz")]
 
 class LikedProduct extends Component
 {
