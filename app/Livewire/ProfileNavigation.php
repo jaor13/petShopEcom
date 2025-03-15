928937c39578ef0a7ba6e7b091f371fd82d7792f
Laiza <hediphonextra@gmail.com>
2025-03-15 15:12:17 +0800

    link to liked products working
    - but to be fixed

--- app/Livewire/ProfileNavigation.php
+++ app/Livewire/ProfileNavigation.php
@@ -9,11 +9,16 @@ class ProfileNavigation extends Component
 
     public $activeSection = 'my-account';
 
+    public function mount()
+    {
+        $this->activeSection = request()->query('section', 'my-account');
+    }
+
     public function setActiveSection($section)
     {
         $this->activeSection = $section;
     }
-        public function render()
+    public function render()
     {
         return view('livewire.profile-navigation');
     }
