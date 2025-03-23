9986706074677476331c1806faddb1bf7be82287
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-23 11:38:31 +0800

    update chat blade

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,3 +1,15 @@
+
 <div>
-    {{-- The whole world belongs to you. --}}
+    <div style="height: 300px; overflow-y: auto;">
+        @foreach ($messages as $msg)
+            <div class="{{ $msg->sender_id == auth()->id() ? 'text-right' : 'text-left' }}">
+                <strong>{{ $msg->sender->name }}:</strong> {{ $msg->message }}
+            </div>
+        @endforeach
+    </div>
+
+    <input type="text" wire:model="message" placeholder="Type a message..." />
+    <button wire:click="sendMessage">Send</button>
 </div>
+
+
