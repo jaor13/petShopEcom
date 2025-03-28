68726bf05fd574941b508490f6aa3bb312c28034
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-28 22:40:05 +0800

    img-button

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -2,14 +2,15 @@
     {{-- Because she competes with no one, no one can compete with her. --}}
 
     @if ($selectedConversation)
-        <form wire:submit.prevent="sendMessage" action="">
+        <form wire:submit.prevent="sendMessage" class="chat-form">
             <div class="chatbox_footer">
-            <div class="custom_form_group">
+                <div class="custom_form_group">
+                    <button type="button" class="image-button">
+                        <i class="fas fa-image"></i>
+                    </button>
                     <input wire:model.defer="body" placeholder="Write message" type="text" class="control">
                     <button type="submit" class="submit">Send</button>
                 </div>
-
-
             </div>
         </form>
 
