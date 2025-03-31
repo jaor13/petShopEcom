b0d25f174fdfdd06e8747bba43486078f8e8928c
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-31 11:30:10 +0800

    mb, ml

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
 <div class="container mx-auto px-4">
-    <h1 class="text-2xl font-semibold mb-4 " style="color: #4F4F4F;">Shopping Cart</h1>
+    <h1 class="text-2xl font-semibold mb-1 " style="color: #4F4F4F;">Shopping Cart</h1>
     <div class="flex flex-col md:flex-row gap-4">
       <div class="md:w-3/4 mb-4">
         <div>
@@ -120,7 +120,7 @@
           </div>
         </div>
         </div>
-      <div class="md:w-1/4">
+      <div class="md:w-1/4 -ml-5">
         <div class="bg-white rounded-md shadow-md p-6">
           <h2 class="text-lg font-semibold mb-4 text-[#4F4F4F]">Summary</h2>
           <div class="flex justify-between mb-2">
