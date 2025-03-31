3e528f05449ecda8a190d1daafd7017323bd186d
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2025-03-31 11:08:24 +0800

    mt, mb

--- resources/views/livewire/profile-navigation.blade.php
+++ resources/views/livewire/profile-navigation.blade.php
@@ -1,16 +1,16 @@
 <div class="row main-box ">
     <nav id="" class="col-md-4 col-lg-3 d-md-block  p-1">
     <div class="{{ $activeSection === 'my-account' ? '' : 'd-none' }}">
-    <h4 class="section-title " style="color: #4F4F4F; margin-bottom: -0.64em;">Manage Account Details</h4>
+    <h4 class="section-title " style="color: #4F4F4F; margin-bottom: -0.68em;">Manage Account Details</h4>
     </div>
     <div class="{{ $activeSection === 'my-purchases' ? '' : 'd-none' }}">
-    <h4 class="section-title"  style="color: #4F4F4F; margin-bottom: -0.64em;">My Purchases</h4>
+    <h4 class="section-title"  style="color: #4F4F4F; margin-bottom: -0.68em;">My Purchases</h4>
     </div>
     <div class="{{ $activeSection === 'ratings-reviews' ? '' : 'd-none' }}">
-    <h4 class="section-title" style="color: #4F4F4F; margin-bottom: -0.64em;">Ratings and Reviews</h4>
+    <h4 class="section-title" style="color: #4F4F4F; margin-bottom: -0.68em;">Ratings and Reviews</h4>
     </div>
     <div class="{{ $activeSection === 'liked-product' ? '' : 'd-none' }}">
-    <h4 class="section-title" style="color: #4F4F4F;margin-bottom: -0.64em;">My Likes</h4>
+    <h4 class="section-title" style="color: #4F4F4F;margin-bottom: -0.68em;">My Likes</h4>
     </div>
         <div class="position-sticky bg-white">
             @livewire('profile-settings')
